22c8c36ae86dc8d6c6454588b90fab1c9b0c0693
wangwanggogo <wangwanggogo19xx@gmail.com>
2018-03-18 22:10:24 +0800

    something

---
+++ myexamples/user_mgr/user_mgr.lua
@@ -0,0 +1,14 @@
+local skynet = require "skynet"
+
+local USER = {}
+
+local function USER.login(username,password)
+
+end
+
+local function USER.register(username,password,...)
+	-- body
+end
+
+
+
